db94dae5169b25478c4d7efe127360ba7ad67434
ZynXx <techigwe@gmail.com>
2026-03-06 21:08:28 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+require 'config.php';
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>Llamabuzz - Accueil</title>
+<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<header>
+<h1>Bienvenue sur Llamabuzz</h1>
+<nav>
+<a href="register.php">S'inscrire</a>
+<a href="login.php">Se connecter</a>
+<a href="rules.php">Règles</a>
+<a href="ranking.php">Classement</a>
+</nav>
+</header>
+<main>
+<p>Votez pour votre candidat préféré et gagnez des prix incroyables !</p>
+</main>
+</body>
+</html>
