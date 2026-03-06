59ab20de2770ae59ad7311e42470efd81c3010bb
ZynXx <techigwe@gmail.com>
2026-03-06 21:07:53 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+require __DIR__ . '/vendor/autoload.php';
+
+use Supabase\SupabaseClient;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$supabase = new SupabaseClient($_ENV['SUPABASE_URL'], $_ENV['SUPABASE_KEY']);
